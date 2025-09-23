78fa62c62318bde8c9e0c075efdbeb0ea3fe9016
Your Name <you@example.com>
2025-09-23 13:23:12 +0530

    checkpoint

--- src/core/views/user_view.py
+++ src/core/views/user_view.py
@@ -1,7 +1,6 @@
 import logging
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
